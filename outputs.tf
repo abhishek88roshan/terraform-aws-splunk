fed3757498ce8fc4911799142bcb74a7f481289c
EngrAbhishekRoshan <abhishek88roshan@yahoo.com>
2025-07-08 02:11:19 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "splunk_public_url" {
+output "splunk_url" {
   value = "http://${aws_instance.splunk.public_ip}:8000"
+}
+
+output "key_pair_name" {
+  value = aws_key_pair.splunk_key.key_name
+}
+
+output "security_group_name" {
+  value = aws_security_group.splunk_sg.name
 }
